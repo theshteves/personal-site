4c6cce530aad657c116ca19ab1eb9beb89942302
theshteves <kneisers@msu.edu>
2016-05-10 00:34:14 -0400

    Github projects now dynamically shown based on popularity\!

--- app_home/views.py
+++ app_home/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from app_home.models import Project
 from django.http import HttpResponse
+from django.shortcuts import render
+import json
+import requests
 
-# Create your views here.
+
 def home(request):
-    #return HttpResponse("Hello World. You're at the index index.")
-    return render(request, "home/index.html")
+    projects = requests.get("https://api.github.com/users/theshteves/repos")
+    projects = json.dumps(projects.json())
+    projects = json.loads(projects)
+    
+    popular = [proj["full_name"] for proj in projects if int(proj["stargazers_count"]) > 0]
+    [Project.objects.create(name=x) for x in popular]
+    """
+    for proj in projects:
+        if int(proj["stargazers_count"]) > 1:
+            Project.objects.create(name=proj["full_name"])
+    """
+    return render(request, "home/index.html", {"projects": Project.objects.all()})
