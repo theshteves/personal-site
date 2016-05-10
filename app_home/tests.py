99d694512b925dc6805004189175dd4d7ae7508f
theshteves <kneisers@msu.edu>
2016-05-10 00:25:27 -0400

    Fixed GitHub API call testcase

--- app_home/tests.py
+++ app_home/tests.py
@@ -1,9 +1,12 @@
 from django.test import Client, TestCase
 from django.contrib.staticfiles import finders
+import requests
 
 client = Client()
 
-class ViewTestCase(TestCase):
+class TestCase1(TestCase):
+    """ Verify home's index assests are linked """
+
     def setUp(self):
         self.response = client.get("#")
         self.found_css = finders.find("css/home_index.css")
@@ -11,8 +14,16 @@ class ViewTestCase(TestCase):
         self.found_js = finders.find("js/home_index.js")
 
     def test_home_index(self):
-        """ Verify home's index assests are linked """
         self.assertEqual(self.response.status_code, 200)
         self.assertEqual(not self.found_css, False)
         self.assertEqual(not self.found_img, False)
         self.assertEqual(not self.found_js, False)
+
+class TestCase2(TestCase):
+    """ Verify GitHub API call for projects """
+
+    def setUp(self):
+        self.response = requests.get("https://api.github.com/users/theshteves/repos")
+
+    def test_github_request(self):
+        self.assertEqual(self.response.status_code, 200)
