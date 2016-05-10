0c5e785f22a390931cd10fcf7736c78a773b9b20
theshteves <kneisers@msu.edu>
2016-05-10 00:29:13 -0400

    Updated app_home db schema

--- app_home/models.py
+++ app_home/models.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    #description = models.CharField(max_length=100)
+    #link = models.CharField(max_length=200)
